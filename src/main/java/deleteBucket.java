50e927da6c4ef820d5ff6263eb92176d4ab31ea5
sahil dholpuria <dholpuria1999@gmail.com>
2021-06-21 10:29:34 +0530

    Initial commit

---
+++ src/main/java/deleteBucket.java
@@ -0,0 +1,38 @@
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.DeleteBucketRequest;
+import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
+import software.amazon.awssdk.services.s3.model.S3Exception;
+
+public class deleteBucket {
+    public static void main(String[] args) {
+
+        Region region = Region.US_WEST_2;
+        S3Client s3 = S3Client.builder().region(region).build();
+        String bucket = "bucket1624212007043";
+        String key = "key";
+
+        cleanUp(s3,bucket,key);
+
+    }
+
+    public static void cleanUp(S3Client s3Client, String bucketName, String keyName) {
+        System.out.println("Cleaning up...");
+        try {
+            System.out.println("Deleting object: " + keyName);
+            DeleteObjectRequest deleteObjectRequest = DeleteObjectRequest.builder().bucket(bucketName).key(keyName).build();
+            s3Client.deleteObject(deleteObjectRequest);
+            System.out.println(keyName +" has been deleted.");
+            System.out.println("Deleting bucket: " + bucketName);
+            DeleteBucketRequest deleteBucketRequest = DeleteBucketRequest.builder().bucket(bucketName).build();
+            s3Client.deleteBucket(deleteBucketRequest);
+            System.out.println(bucketName +" has been deleted.");
+            System.out.printf("%n");
+        } catch (S3Exception e) {
+            System.err.println(e.awsErrorDetails().errorMessage());
+            System.exit(1);
+        }
+        System.out.println("Cleanup complete");
+        System.out.printf("%n");
+    }
+}
